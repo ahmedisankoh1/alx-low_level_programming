072dd7d801ef01951918cba92eb05775f00e6dfe
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-09-07 16:54:21 -0700

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unstdio.h>
 /**
  * main - A ptogramme with a new line
  * Return: 1 (success)
  */
 int main(void)
 {
-	fwrite(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
