974c134cb98d4326ca3275bb628a03d3f233bb87
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-09-07 17:08:22 -0700

    result

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * main - A ptogramme with a new line
+ * main - A programme with a new line
  * Return: 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
