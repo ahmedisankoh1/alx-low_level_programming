575be03d6fab365ef8b9afb2b1c3a16259cac653
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-09-08 18:45:24 -0700

    time

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%i", i);
+	printf("\n");
+	return (0);
+}
