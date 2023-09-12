bd946cc307b882725066d183255915bcb6ef8ded
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-09-12 16:45:36 -0700

    jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * jack_bauer - Soundtrack time
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 24 ; i++)
+	{
+		for (j = 0 ; j < 60 ; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+	}
+}
