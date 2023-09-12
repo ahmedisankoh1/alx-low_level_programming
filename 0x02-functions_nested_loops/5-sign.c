ea52a8dcd2a30fbefef70549374ceea21ada96d2
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-09-12 15:52:47 -0700

    printing_signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - printing signs
+ * @n: number to be checked
+ * Return: always 1 or 0 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
