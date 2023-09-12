af6ce0b78c01652938c856fa3a483f1bfc0cb0d2
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-09-12 12:01:09 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabete - Printing alphabet
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar('i');
+	_putchar('\n');
+
+}
