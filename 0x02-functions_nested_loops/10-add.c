86229e70919de7dec997784061bdbb88ff838af3
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-09-12 17:24:34 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: secont int
+ * Return: always a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * times_table - times table
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			result = i * j;
+			_putchar(result = '0');
+		}
+	}
+}
