d85340a7b0bdff61634dedd4447bb166e5a129c0
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-09-21 16:01:12 -0700

    Resoved

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -32,3 +32,10 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+    print_array(a, sizeof(a) / sizeof(int));
+    reverse_array(a, sizeof(a) / sizeof(int));
+    print_array(a, sizeof(a) / sizeof(int));
+    return (0);
+}
