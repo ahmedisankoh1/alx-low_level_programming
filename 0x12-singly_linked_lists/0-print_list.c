26aa4cab8db7b4ac359843a72433daed174326a9
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-19 16:23:00 -0700

    please

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t a = 0;
+	size_t s = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		a++;
+		s++;
 	}
 
-	return (a);
+	return (s);
 }
