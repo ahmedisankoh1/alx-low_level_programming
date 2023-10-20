a13de51677f8e2bb4218506343edc3805b9a07c5
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-20 15:17:26 -0700

    test

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+int print_before_main(void) __attribute__((constructor));
+
+/**
+ * print_before_main - A function that executes before the main function
+ *
+ * Return: success 0
+ */
+
+int print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+	return (0);
+}
