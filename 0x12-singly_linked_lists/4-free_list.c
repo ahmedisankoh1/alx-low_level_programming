6a3376885172d50ad5a991331f2b0291d3bb37e2
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-20 16:24:44 -0700

    please

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
+/**
+ * free_list - Free the list_t list
+ * @head: list_t list that is to be freed
+ *
+ * Return: returns nothing
+ */
+
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
 }
