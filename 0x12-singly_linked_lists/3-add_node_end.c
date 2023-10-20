8caa270ba3718d7a05b8fee7c47cd1e20ca7f98d
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-20 13:57:23 -0700

    test

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,35 +4,35 @@
  * @head: double pointer to the list_t list
  * @str: string to put in the new node
  *
- * Return: Return NULL if it failed
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
-	list_t *tmp = *head;
-	unsigned int length = 0;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	while (str[length])
-		length++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = length;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new;
-		return (NULL);
+		return (new);
 	}
 
-	while (tmp->next != NULL)
-		tmp = tmp->next;
+	while (temp->next)
+		temp = temp->next;
 
-	tmp->next = new;
+	temp->next = new;
 
-	return (NULL);
+	return (new);
 }
