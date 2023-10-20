35ff7d05a1aba24911a69f7cf979e46bf675981c
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-20 15:43:44 -0700

    test

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,7 +9,8 @@ int print_before_main(void) __attribute__((constructor));
 
 int print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 
 	return (0);
 }
