5b5473b5a2d0b19ec7b9ad0d5f5f9daeefa1be7a
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-24 16:25:10 -0700

    getting_it

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
