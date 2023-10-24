f9d29a2c51ca678f07193e2c9ba45ae8fdaefdc6
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-24 05:27:32 -0700

    test

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free's the listint_t list and initiallize head to NULL
+ * @head: The head of the listint_t list
+ *
+ * Return: returns void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *check;
+
+
+	if (*head == NULL)
+		free(*head);
+
+	while (*head != NULL)
+	{
+		check = (*head)->next;
+		free(*head);
+		*head = check;
+	}
+	*head = NULL;
+}
