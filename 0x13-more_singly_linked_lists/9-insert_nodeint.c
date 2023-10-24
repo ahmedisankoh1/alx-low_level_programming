036808adb59758628b46f8ddfcd325e42bf9815f
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-24 15:14:27 -0700

    yest

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	new_node->n = n;
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
 	for (i = 0; i < a; i++)
 	{
