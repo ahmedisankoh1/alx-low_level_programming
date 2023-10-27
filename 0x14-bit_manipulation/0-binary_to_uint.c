e07f5f39588c5609f254dd2a900e5428b7ada485
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-10-27 14:00:29 -0700

    test

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary to an int
+ * @b: it contains the binary to be converted
+ *
+ * Return: the int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int power = 1;
+	int i;
+	int result = 0;
+
+	while (b[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		if (b[i] == '1')
+			result = result + power;
+		power = power * 2;
+	}
+	return (result);
+}
