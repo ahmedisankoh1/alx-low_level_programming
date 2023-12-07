5c1999d20b0597e62dda2fddf1be706ad587f900
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-12-07 14:22:52 -0800

    hi

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,8 +9,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	if (h->next == NULL)
-		return (0);
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
+		h= h->prev;
+
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
